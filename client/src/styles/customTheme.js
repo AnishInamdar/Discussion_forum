a9af699d56d7a7b005c9a38b60633b4a145758c3
AnishInamdar <2019anish.inamdar@ves.ac.in>
2022-11-01 20:49:38 +0530

    Initial commit

---
+++ client/src/styles/customTheme.js
@@ -0,0 +1,51 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const customTheme = (darkMode) =>
+  createMuiTheme({
+    palette: {
+      type: darkMode ? 'dark' : 'light',
+      primary: {
+        main: darkMode ? '#7289de' : '#964ec2',
+      },
+      secondary: {
+        main: darkMode ? '#7289da' : '#562873',
+      },
+    },
+    overrides: {
+      MuiMenuItem: {
+        root: {   
+          '&$selected': {
+            borderRight: '4px solid #f90',
+            fontWeight: '700',
+          },
+        },
+      },
+      MuiPopover: {
+        paper: {
+          borderRadius: 2,
+        },
+      },
+      MuiButton: {
+        root: {
+          borderRadius: 4,
+          textTransform: 'none',
+        },
+      },
+      MuiChip: {
+        root: {
+          borderRadius: 3,
+          padding: '0px',
+        },
+        outlined: {
+          backgroundColor: darkMode ? '#f4f4f4' : '#964ec215',
+        },
+      },
+    },
+    props: {
+      MuiButton: {
+        disableElevation: true,
+      },
+    },
+  });
+
+export default customTheme;
